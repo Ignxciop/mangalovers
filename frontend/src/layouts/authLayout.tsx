f621028a58f23a3e181455b4bb53075a6c037867
José Núñez <josenunezm2001@gmail.com>
2026-02-18 00:29:54 -0300

    Páginas de inicio de sesión y registro más componentes de shadcn

---
+++ frontend/src/layouts/authLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+export default function AuthLayout() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-grey-100 to-grey-300 dark:from-gray-900 dark:to-gray-800">
+            <Outlet />
+        </div>
+    );
+}
---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,74 @@
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+    return (
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Register to your account</CardTitle>
+                <CardDescription>
+                    Enter your email below to register to your account
+                </CardDescription>
+                <CardAction>
+                    <Link to="/login">
+                        <Button variant="link">Sign In</Button>
+                    </Link>
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name">Name</Label>
+                            <Input
+                                id="email"
+                                type="input"
+                                placeholder="Name Lastname"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="your@email.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">
+                                    Repeat Password
+                                </Label>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full">
+                    Register
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
