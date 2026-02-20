ba126a5dc73c3d665c35bfec06a8f3e0ebb4b69c
José Núñez <josenunezm2001@gmail.com>
2026-02-20 00:10:25 -0300

    Creación de mainLayout para cuentas autenticadas con página dashboard(vacía) + sidebar para layout

--- frontend/src/components/layouts/mainLayout.tsx
+++ frontend/src/components/layouts/mainLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function MainLayout() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-grey-100 to-grey-300 dark:from-gray-900 dark:to-gray-800">
-            <Outlet />
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <div className="flex-auto bg-gradient-to-br from-grey-100 to-grey-300 dark:from-gray-900 dark:to-gray-800">
+                <SidebarTrigger />
+                <Outlet />
+            </div>
+        </SidebarProvider>
     );
 }
