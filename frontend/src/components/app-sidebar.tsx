0c79554974f57010a765c046dfdfdd2f471d40f3
José Núñez <josenunezm2001@gmail.com>
2026-02-20 03:00:00 -0300

    Sidebar mejorado, ahora mostrando nombre de la app y una sección de usuario con dropdown

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -2,19 +2,106 @@ import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
-    SidebarGroup,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/useAuth";
+import { Dumbbell, UserRound, EllipsisVertical } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
 
 export function AppSidebar() {
+    const { logout, user } = useAuth();
+
     return (
         <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+            <SidebarHeader className="mb-4 items-center">
+                <a href="#" className="flex gap-1 items-center">
+                    <Dumbbell className="size-5" />
+                    <span className="font-semibold text-[20px]">Gymlovers</span>
+                </a>
+            </SidebarHeader>
+            <SidebarContent className="ml-4">
+                <div>Navegación</div>
             </SidebarContent>
-            <SidebarFooter />
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton
+                                    size="lg"
+                                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                                >
+                                    <Avatar className="h-8 w-8 rounded-lg grayscale">
+                                        <AvatarImage>
+                                            <UserRound />
+                                        </AvatarImage>
+                                    </Avatar>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="truncate font-medium">
+                                            {user?.name}
+                                        </span>
+                                        <span className="text-muted-foreground truncate text-xs">
+                                            {user?.email}
+                                        </span>
+                                    </div>
+                                    <EllipsisVertical className="ml-auto size-4" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                                side={"bottom"}
+                                align="end"
+                                sideOffset={4}
+                            >
+                                <DropdownMenuLabel className="p-0 font-normal">
+                                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                        <Avatar className="h-8 w-8 rounded-lg">
+                                            <AvatarImage>
+                                                <UserRound />
+                                            </AvatarImage>
+                                        </Avatar>
+                                        <div className="grid flex-1 text-left text-sm leading-tight">
+                                            <span className="truncate font-medium">
+                                                {user?.name}
+                                            </span>
+                                            <span className="text-muted-foreground truncate text-xs">
+                                                {user?.email}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <ModeToggle></ModeToggle>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        Notifications
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={logout}>
+                                    Log out
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
         </Sidebar>
     );
 }
